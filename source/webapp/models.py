5619aafa3d09e3ead70a6dd8eee0497f6120c5ca
Yuburov <yubur.aziz@gmail.com>
2019-10-19 12:25:55 +0600

    Создал модели Poll и Choise. Применил миграции.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.TextField(max_length=2000,null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def _str__(self):
+        return self.question
+
+
+
+class Choise(models.Model):
+    text = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Текст')
+    poll = models.ForeignKey('webapp.Poll',related_name='choises', on_delete=models.CASCADE, verbose_name='Опрос' )
+
+    def __str__(self):
+        return self.text
